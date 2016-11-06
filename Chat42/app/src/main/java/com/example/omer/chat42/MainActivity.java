11a4e412e29641fca5ca7bfee215386780da7f42
Omer <elomer@gmail.com>
2016-11-06 15:06:28 +0200

    menu bar

---
+++ Chat42/app/src/main/java/com/example/omer/chat42/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.omer.chat42;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
