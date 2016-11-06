91db230c8ebf7de6dd8f3e1607d1c95f6d3bbb53
Omer <elomer@gmail.com>
2016-11-06 15:17:42 +0200

    menu

--- Chat42/app/src/main/java/com/example/omer/chat42/MainActivity.java
+++ Chat42/app/src/main/java/com/example/omer/chat42/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.omer.chat42;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    
+    // Make the action button appear in old devices
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
